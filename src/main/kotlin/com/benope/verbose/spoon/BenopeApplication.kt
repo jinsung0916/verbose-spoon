9c5384debf860141085ce79b15bc29acfa32f4b2
AJ <jinsung0916@naver.com>
2021-09-04 20:12:47 +0900

    Template 프로젝트 수정사항 반영
    - Enable audit entity

--- src/main/kotlin/com/benope/verbose/spoon/BenopeApplication.kt
+++ src/main/kotlin/com/benope/verbose/spoon/BenopeApplication.kt
@@ -2,8 +2,10 @@ package com.benope.verbose.spoon;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @SpringBootApplication
+@EnableJpaAuditing
 class BenopeApplication
 
 fun main(args: Array<String>) {
--- src/main/kotlin/com/benope/verbose/spoon/core_backend/common/domain/AuditEntity.kt
+++ src/main/kotlin/com/benope/verbose/spoon/core_backend/common/domain/AuditEntity.kt
@@ -1,22 +1,30 @@
 package com.benope.verbose.spoon.core_backend.common.domain
 
+import org.springframework.data.annotation.CreatedBy
 import org.springframework.data.annotation.CreatedDate
+import org.springframework.data.annotation.LastModifiedBy
+import org.springframework.data.annotation.LastModifiedDate
 import org.springframework.data.domain.AbstractAggregateRoot
+import org.springframework.data.jpa.domain.support.AuditingEntityListener
 import java.time.LocalDateTime
+import javax.persistence.EntityListeners
 import javax.persistence.MappedSuperclass
 import javax.persistence.Version
 
 @MappedSuperclass
+@EntityListeners(AuditingEntityListener::class)
 open class AuditEntity<A : AbstractAggregateRoot<A>?> : AbstractAggregateRoot<A>() {
 
     @CreatedDate
     private var createDateTime: LocalDateTime? = null
 
+    @CreatedBy
     private var createdBy: Long? = null
 
-    @CreatedDate
+    @LastModifiedDate
     private var lastModifiedDatetime: LocalDateTime? = null
 
+    @LastModifiedBy
     private var lastModifiedBy: Long? = null
 
     // Version property for optimistic locking.
