97e74a67ab1058cce1843103d48265ba2c72e73a
AJ <jinsung0916@naver.com>
2021-09-10 15:38:44 +0900

    Bean Configuration 위치 변경
    - MessageSource 설정 WebMvcConfig.kt 로 이동

--- src/main/kotlin/com/benope/verbose/spoon/core_backend/common/config/BeanConfig.kt
+++ src/main/kotlin/com/benope/verbose/spoon/core_backend/common/config/BeanConfig.kt
@@ -1,10 +1,9 @@
 package com.benope.verbose.spoon.core_backend.common.config
 
 import org.modelmapper.ModelMapper
-import org.springframework.context.MessageSource
+import org.modelmapper.convention.MatchingStrategies
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
-import org.springframework.context.support.ReloadableResourceBundleMessageSource
 
 
 @Configuration
@@ -15,23 +14,12 @@ class BeanConfig {
         val modelMapper = ModelMapper()
         modelMapper
             .configuration
+            .setMatchingStrategy(MatchingStrategies.STRICT)
             .setFieldMatchingEnabled(true)
             .setFieldAccessLevel(org.modelmapper.config.Configuration.AccessLevel.PRIVATE)
             .setDeepCopyEnabled(true)
             .setSkipNullEnabled(true)
         return modelMapper
     }
-
-    @Bean
-    fun messageSource(): MessageSource {
-        val messageSource = ReloadableResourceBundleMessageSource()
-        messageSource.setDefaultEncoding("UTF-8") // 인코딩 설정
-        messageSource.setBasenames(
-            "classpath:message/messages",
-            "classpath:org/springframework/security/messages"
-        )
-        return messageSource
-    }
-
 
 }
--- src/main/kotlin/com/benope/verbose/spoon/core_backend/common/config/WebMvcConfig.kt
+++ src/main/kotlin/com/benope/verbose/spoon/core_backend/common/config/WebMvcConfig.kt
@@ -1,9 +1,23 @@
 package com.benope.verbose.spoon.core_backend.common.config
 
+import org.springframework.context.MessageSource
+import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
+import org.springframework.context.support.ReloadableResourceBundleMessageSource
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer
 
 @Configuration
 class WebMvcConfig : WebMvcConfigurer {
+
+    @Bean
+    fun messageSource(): MessageSource {
+        val messageSource = ReloadableResourceBundleMessageSource()
+        messageSource.setDefaultEncoding("UTF-8") // 인코딩 설정
+        messageSource.setBasenames(
+            "classpath:message/messages",
+            "classpath:org/springframework/security/messages"
+        )
+        return messageSource
+    }
 
 }
