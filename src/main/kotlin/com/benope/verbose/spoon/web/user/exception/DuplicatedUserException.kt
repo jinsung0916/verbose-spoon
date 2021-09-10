ab746c7ce4f26c3e6fb7c9e508ae0ebe9c018c43
AJ <jinsung0916@naver.com>
2021-09-10 15:27:13 +0900

    아이디 중복 시 400 반환 처리

--- src/main/kotlin/com/benope/verbose/spoon/web/user/exception/DuplicatedUserException.kt
+++ src/main/kotlin/com/benope/verbose/spoon/web/user/exception/DuplicatedUserException.kt
@@ -1,5 +1,8 @@
 package com.benope.verbose.spoon.web.user.exception
 
 import com.benope.verbose.spoon.core_backend.common.exception.LocalizedException
+import org.springframework.http.HttpStatus
+import org.springframework.web.bind.annotation.ResponseStatus
 
+@ResponseStatus(HttpStatus.BAD_REQUEST)
 class DuplicatedUserException : LocalizedException("exception.user.duplicated")
