88b0b8f37ed0da0ce956a5042efa6b7caf55ca51
AJ <jinsung0916@naver.com>
2021-09-20 01:33:55 +0900

    사용자 정보 반환 시 userId, authority 반환 처리

--- src/main/kotlin/com/benope/verbose/spoon/web/user/dto/UserResponse.kt
+++ src/main/kotlin/com/benope/verbose/spoon/web/user/dto/UserResponse.kt
@@ -3,20 +3,24 @@ package com.benope.verbose.spoon.web.user.dto
 import com.benope.verbose.spoon.core_backend.security.domain.User
 
 data class UserResponse(
+    var userId: Long?,
     var username: String?,
     var firstName: String?,
     var lastName: String?,
     var nickname: String?,
     var email: String?,
+    var authority: Set<String>?
 ) {
     companion object {
         fun fromUser(user: User?): UserResponse {
             return UserResponse(
+                userId = user?.userId,
                 username = user?.username,
                 firstName = user?.name?.firstName,
                 lastName = user?.name?.lastName,
                 nickname = user?.nickname?.value,
-                email = user?.email?.value
+                email = user?.email?.value,
+                authority = user?.authorities?.map { it.authority }?.toSet()
             )
         }
     }
