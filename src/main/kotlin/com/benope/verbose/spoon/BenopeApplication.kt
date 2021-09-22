ec5e7751763d53d49cee0f09257d4b1a2d5f9b30
AJ <jinsung0916@naver.com>
2021-09-22 17:08:18 +0900

    External tomcat 배포를 위한 WAR 패키징 설정

--- src/main/kotlin/com/benope/verbose/spoon/BenopeApplication.kt
+++ src/main/kotlin/com/benope/verbose/spoon/BenopeApplication.kt
@@ -1,12 +1,14 @@
 package com.benope.verbose.spoon;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.boot.runApplication
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @SpringBootApplication
 @EnableJpaAuditing
-class BenopeApplication
+class BenopeApplication : SpringBootServletInitializer()
 
 fun main(args: Array<String>) {
     runApplication<BenopeApplication>(*args)
