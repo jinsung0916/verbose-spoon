5e5fe8673cf0337142f9f49746f020286b136965
AJ <jinsung0916@naver.com>
2021-09-24 22:33:31 +0900

    로컬 개발 환경 구성을 위한 Inmemory DB 설정 적용

--- src/main/kotlin/com/benope/verbose/spoon/BenopeApplication.kt
+++ src/main/kotlin/com/benope/verbose/spoon/BenopeApplication.kt
@@ -1,14 +1,41 @@
 package com.benope.verbose.spoon;
 
+import com.benope.verbose.spoon.web.user.dto.CreateUserRequest
+import com.benope.verbose.spoon.web.user.service.UserManageService
 import org.springframework.boot.autoconfigure.SpringBootApplication
-import org.springframework.boot.builder.SpringApplicationBuilder
 import org.springframework.boot.runApplication
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer
+import org.springframework.context.annotation.Configuration
+import org.springframework.context.annotation.Profile
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing
+import javax.annotation.PostConstruct
 
 @SpringBootApplication
 @EnableJpaAuditing
-class BenopeApplication : SpringBootServletInitializer()
+class BenopeApplication : SpringBootServletInitializer() {
+
+    @Configuration
+    @Profile("local")
+    class IntegrationTestPostConstruct(
+        private val userManageService: UserManageService
+    ) {
+
+        @PostConstruct
+        fun postConstruct() {
+            val createUserRequest = CreateUserRequest(
+                username = "username",
+                password = "password",
+                firstName = "firstName",
+                lastName = "lastName",
+                nickname = "nickname",
+                email = "email@benope.com"
+            )
+
+            userManageService.createUser(createUserRequest)
+        }
+    }
+
+}
 
 fun main(args: Array<String>) {
     runApplication<BenopeApplication>(*args)
