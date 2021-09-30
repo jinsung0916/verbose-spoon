ac8006d716600f87e9cb6c7abba1de1bee112b86
AJ <jinsung0916@naver.com>
2021-09-30 19:15:13 +0900

    Async 처리 중 exception 발생 시 로깅 처리

--- src/main/kotlin/com/benope/verbose/spoon/core_backend/async/exception/CustomAsyncExceptionHandler.kt
+++ src/main/kotlin/com/benope/verbose/spoon/core_backend/async/exception/CustomAsyncExceptionHandler.kt
@@ -1,18 +1,18 @@
 package com.benope.verbose.spoon.core_backend.async.exception
 
+import org.slf4j.Logger
+import org.slf4j.LoggerFactory
 import org.springframework.aop.interceptor.AsyncUncaughtExceptionHandler
 import java.lang.reflect.Method
 
 class CustomAsyncExceptionHandler : AsyncUncaughtExceptionHandler {
 
+    val logger: Logger = LoggerFactory.getLogger(CustomAsyncExceptionHandler::class.java)
+
     override fun handleUncaughtException(
         throwable: Throwable, method: Method, vararg obj: Any
     ) {
-        println("Exception message - " + throwable.message)
-        println("Method name - " + method.getName())
-        for (param in obj) {
-            println("Parameter value - $param")
-        }
+        logger.error("Async exception: ", throwable)
     }
 
 }
